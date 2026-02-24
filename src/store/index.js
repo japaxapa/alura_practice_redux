13d4ab6609bb76d9c2d3789b0688bef8834f2275
Mario Kawakita <mario.jhk@gmail.com>
2026-02-24 14:56:39 -0300

    feat: localStorage state

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import taskReducer from "./slices/taskSlice";
 import themeReducer from "./slices/themeSlice";
 import analyticReducer from "./slices/analyticSlice";
+import { loadState, saveState, throttle } from "./localStorage";
+
+const preloadedState = loadState();
 
 export const store = configureStore({
   reducer: {
@@ -9,4 +12,18 @@ export const store = configureStore({
     theme: themeReducer,
     analytics: analyticReducer,
   },
+  devTools: process.env.NODE_ENV !== "production",
+  preloadedState,
+});
+
+const throttleSavestate = throttle((state) => {
+  saveState({
+    tasks: state.tasks,
+    theme: state.theme,
+    analytics: state.analytics,
+  });
+}, 1000);
+
+store.subscribe(() => {
+  throttleSavestate(store.getState());
 });
